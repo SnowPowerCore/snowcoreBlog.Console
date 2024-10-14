b1b0d0bca7d9e12b2e1fb695058d0cd43e913e4d
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-10-14 23:15:34 +0300

    - Fixes;

--- App/Steps/Launch/EveryTime/HandleLaunchErrorsStep.cs
+++ App/Steps/Launch/EveryTime/HandleLaunchErrorsStep.cs
@@ -1,21 +1,23 @@
 ﻿using MinimalStepifiedSystem.Interfaces;
 using snowcoreBlog.ApplicationLaunch.Context;
 using snowcoreBlog.ApplicationLaunch.Delegates;
+using snowcoreBlog.ApplicationLaunch.Models;
 using snowcoreBlog.Console.App.Interfaces;
 
 namespace snowcoreBlog.Console.App.Steps.Launch.EveryTime;
 
-public class HandleLaunchErrorsStep(IConsoleApplicationService application) : IStep<LaunchDelegate, LaunchContext>
+public class HandleLaunchErrorsStep(IConsoleApplicationService application) : IStep<LaunchDelegate, LaunchContext, ApplicationLaunchResult>
 {
-    public async Task InvokeAsync(LaunchContext context, LaunchDelegate next, CancellationToken token = default)
+    public async Task<ApplicationLaunchResult> InvokeAsync(LaunchContext context, LaunchDelegate next, CancellationToken token = default)
     {
         try
         {
-            await next(context);
+            return await next(context, token);
         }
         catch (Exception ex)
         {
             application.Logging.TrackException(ex);
+            return new();
         }
     }
 }
--- App/Steps/Launch/EveryTime/NavigateToRootScreenStep.cs
+++ App/Steps/Launch/EveryTime/NavigateToRootScreenStep.cs
@@ -1,15 +1,16 @@
 ﻿using MinimalStepifiedSystem.Interfaces;
 using snowcoreBlog.ApplicationLaunch.Context;
 using snowcoreBlog.ApplicationLaunch.Delegates;
+using snowcoreBlog.ApplicationLaunch.Models;
 using snowcoreBlog.Console.App.Interfaces;
 
 namespace snowcoreBlog.Console.App.Steps.Launch.EveryTime;
 
-public class NavigateToRootScreenStep(IConsoleNavigationService navigation) : IStep<LaunchDelegate, LaunchContext>
+public class NavigateToRootScreenStep(IConsoleNavigationService navigation) : IStep<LaunchDelegate, LaunchContext, ApplicationLaunchResult>
 {
-    public async Task InvokeAsync(LaunchContext context, LaunchDelegate next, CancellationToken token = default)
+    public async Task<ApplicationLaunchResult> InvokeAsync(LaunchContext context, LaunchDelegate next, CancellationToken token = default)
     {
         await navigation.NavigateToRootAsync();
-        await next(context);
+        return new();
     }
 }
