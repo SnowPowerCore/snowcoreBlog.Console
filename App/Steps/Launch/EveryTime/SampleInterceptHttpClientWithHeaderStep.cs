0b899eced6b91a5d554d3755a66f52937b5f003e
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-10-14 23:15:34 +0300

    - Fixes;

--- App/Steps/Launch/EveryTime/SampleInterceptHttpClientWithHeaderStep.cs
+++ App/Steps/Launch/EveryTime/SampleInterceptHttpClientWithHeaderStep.cs
@@ -1,20 +1,21 @@
 ﻿using MinimalStepifiedSystem.Interfaces;
 using snowcoreBlog.ApplicationLaunch.Context;
 using snowcoreBlog.ApplicationLaunch.Delegates;
+using snowcoreBlog.ApplicationLaunch.Models;
 using snowcoreBlog.HttpClientInterception.Interceptor.HttpClient;
 using snowcoreBlog.HttpClientInterception.Interfaces;
 
 namespace snowcoreBlog.Console.App.Steps.Launch.EveryTime;
 
-public class SampleInterceptHttpClientWithHeaderStep : IStep<LaunchDelegate, LaunchContext>
+public class SampleInterceptHttpClientWithHeaderStep : IStep<LaunchDelegate, LaunchContext, ApplicationLaunchResult>
 {
     public SampleInterceptHttpClientWithHeaderStep(IHttpClientInterceptor httpClientInterceptor)
     {
         httpClientInterceptor.BeforeSendAsync += HttpClientBeforeSendAsync;
     }
 
-    public Task InvokeAsync(LaunchContext context, LaunchDelegate next, CancellationToken token = default) =>
-        next(context);
+    public Task<ApplicationLaunchResult> InvokeAsync(LaunchContext context, LaunchDelegate next, CancellationToken token = default) =>
+        next(context, token);
 
     private Task HttpClientBeforeSendAsync(object sender, HttpClientInterceptorEventArgs e)
     {
