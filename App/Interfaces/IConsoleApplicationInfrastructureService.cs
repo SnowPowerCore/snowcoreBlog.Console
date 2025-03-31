781486c60c47e4a3d6273577d9db2de42d7bf13f
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-04-01 00:21:20 +0300

    - A much simpler & universal way to handle localization;

--- App/Interfaces/IConsoleApplicationInfrastructureService.cs
+++ App/Interfaces/IConsoleApplicationInfrastructureService.cs
@@ -1,13 +1,10 @@
 ﻿using snowcoreBlog.ConsoleHandling.Interfaces;
-using snowcoreBlog.ResourceLoading.Interfaces;
 
 namespace snowcoreBlog.Console.App.Interfaces;
 
 public interface IConsoleApplicationInfrastructureService
 {
     IConsoleNavigationService Navigation { get; }
-
-    IResourceService Resources { get; }
 
     IConsoleService Console { get; }
 }
